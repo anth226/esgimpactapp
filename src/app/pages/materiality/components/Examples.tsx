637d3636a8cfb9a2e5ab6caad6bd360ff7c3abf7
Kenny Lazer <kennylazer10@gmail.com>
2022-02-07 19:21:10 +0200

    Added kanban on examples tab under materiality page

--- src/app/pages/materiality/components/Examples.tsx
+++ src/app/pages/materiality/components/Examples.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
+import {v4 as uuid} from 'uuid'
 import {KTSVG} from '../../../../_metronic/helpers'
 import {
   ChartsWidget1,
@@ -12,10 +14,121 @@ import {
 import MaterialityHeader from '../MaterialityHeader'
 
 export function Examples() {
+  const onDragEnd = (result: any, columns: any, setColumns: any) => {
+    if (!result.destination) return
+
+    const {source, destination} = result
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId]
+      const destColumn = columns[destination.droppableId]
+      const sourceItems = [...sourceColumn.items]
+      const destItems = [...destColumn.items]
+      const [removed] = sourceItems.splice(source.index, 1)
+      destItems.splice(destination.index, 0, removed)
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      })
+    } else {
+      const column = columns[source.droppableId]
+      const copiedItems = [...column.items]
+      const [removed] = copiedItems.splice(source.index, 1)
+      copiedItems.splice(destination.index, 0, removed)
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      })
+    }
+  }
+  const itemsBack = [
+    {id: uuid(), content: 'Play Fifa'},
+    {id: uuid(), content: 'Buy Milk'},
+    {id: uuid(), content: 'Read a book'},
+    {id: uuid(), content: 'Learn Piano'},
+    {id: uuid(), content: 'Call some friends'},
+    {id: uuid(), content: 'Learn VueJs'},
+  ]
+  const columsBack = {
+    [uuid()]: {name: 'In Process', items: itemsBack},
+    [uuid()]: {name: 'Working', items: []},
+    [uuid()]: {name: 'Done', items: []},
+  }
+
+  const [col, setCol] = useState(columsBack)
+
   return (
     <>
       <MaterialityHeader />
-      Put Example Content Here: 
+
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'stretch'}}>
+        <DragDropContext onDragEnd={(result) => onDragEnd(result, col, setCol)}>
+          {Object.entries(col).map(([id, column]) => {
+            return (
+              <div style={{margin: 8, background: '#fff', cursor: 'all-scroll'}}>
+                <Droppable droppableId={id} key={id}>
+                  {(provided, snapshot) => {
+                    return (
+                      <>
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          style={{
+                            padding: 14,
+                            paddingLeft: 24,
+                            paddingRight: 24,
+                            width: 280,
+                          }}
+                        >
+                          <h4 style={{marginBottom: '40px', fontWeight: 500}}>{column.name}</h4>
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable key={item.id} draggableId={item.id} index={index}>
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        borderRadius: '6px',
+                                        cursor: 'all-scroll',
+                                        padding: 12,
+                                        margin: '0 0 12px 0',
+                                        background: 'rgb(242, 242, 242)',
+                                        fontWeight: '600',
+                                        color: '  #5c6ca3',
+                                        boxShadow: '0 0 2px 2px rgba(242, 242, 242, 0.3)',
+                                        ...provided.draggableProps.style,
+                                      }}
+                                    >
+                                      {item.content}
+                                    </div>
+                                  )
+                                }}
+                              </Draggable>
+                            )
+                          })}
+                        </div>
+                      </>
+                    )
+                  }}
+                </Droppable>
+              </div>
+            )
+          })}
+        </DragDropContext>
+      </div>
     </>
   )
 }
