3ea3e3a3a2323402cc4553a3020d4a2e6d6e1df1
Kenny Lazer <kennylazer10@gmail.com>
2022-02-11 14:29:30 +0200

    Introduced google address auto-complete on fields

--- src/app/modules/auth/components/fund-signup/Fund3.tsx
+++ src/app/modules/auth/components/fund-signup/Fund3.tsx
@@ -1,7 +1,9 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 import {Field, ErrorMessage} from 'formik'
 import {Link} from 'react-router-dom'
 import ArrayString from '../../../../components/auth/ArrayString'
+import {geocodeByAddress, getLatLng} from 'react-places-autocomplete'
+import PlaceAutoComplete from '../../../../components/places-auto-complete'
 
 interface IProps {
   openModal: Function
@@ -9,12 +11,25 @@ interface IProps {
 }
 
 const Fund3: FC<IProps> = (props: IProps) => {
+  const [address, setAddress] = useState('')
+
+  // In case we may need coordinates in the future
+  const [coordinates, setCoordinates] = useState({
+    lat: 0,
+    lng: 0,
+  })
+  const handleSelect = async (value: any) => {
+    const results = await geocodeByAddress(value)
+    const latlng = await getLatLng(results[0])
+    setAddress(value)
+    setCoordinates(latlng)
+  }
+
   return (
     <div className='w-100'>
       <div className='pb-10 pb-lg-15'>
         <h2 className='fw-bolder text-dark'>Operations</h2>
       </div>
-
       <div className='fv-row mb-10'>
         <label className='form-label'>
           How many full-time employees work at {props.assumed_name}?
@@ -35,13 +50,14 @@ const Fund3: FC<IProps> = (props: IProps) => {
         <label className='form-label'>
           What is the address of the {props.assumed_name}'s headquarters?
         </label>
-        <Field
-          type='text'
+        <PlaceAutoComplete
+          address={address}
+          handleSelect={handleSelect}
+          setAddress={setAddress}
           name='primary_address'
-          className='form-control form-control-lg form-control-solid'
-          placeholder='Primary Address'
           label='Primary Address'
-        ></Field>
+          placeholder='Primary Address'
+        />
         <div className='text-danger mt-2'>
           <ErrorMessage name='primary_address' />
         </div>
@@ -51,7 +67,9 @@ const Fund3: FC<IProps> = (props: IProps) => {
         <label className='form-label mb-3'>
           What are the addresses of {props.assumed_name}'s additional locations?
         </label>
+
         <Field name='other_addresses' component={ArrayString} />
+
         <div className='text-danger mt-2'>
           <ErrorMessage name='other_addresses' />
         </div>
